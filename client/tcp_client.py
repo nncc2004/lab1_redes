330f71d11eeb09ff7340be1274662d47988b2f1f
Camilo <cvaldebenitog@usm.cl>
2026-04-01 00:45:05 -0300

    Casi terminados cliente y servidor tcp

--- client/tcp_client.py
+++ client/tcp_client.py
@@ -1,8 +1,50 @@
 import socket
+import threading
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 50002))
-client.send("NICK c4m1lo".encode('utf-8'))
-respuesta = client.recv(1024)
-print(f"Servidor dice: {respuesta.decode('utf-8')}")
-client.close()
+def escuchar_servidor(socket_cliente):
+    while True:
+        try:
+            mensaje = socket_cliente.recv(1024)
+            if not mensaje:
+                print("Se perdio la conexion con el servidor")
+                break
+            print(f"{mensaje.decode('utf-8').strip()}\n")
+            if mensaje.decode('utf-8').strip().startswith("Se finalizo "):
+                break
+        except Exception as e:
+            print(f"Error de conexion: {e}")
+            break
+
+def iniciar_cliente():
+    IP = "127.0.0.1"
+    PUERTO = 50004
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        cliente.connect((IP, PUERTO))
+    except ConnectionRefusedError as e:
+        print(f"No se pudo conectar al servidor: {e}")
+        return
+    print("Ingrese el comando NICK para conectarse a la sala de chat")
+    comando = input()
+    cliente.send(comando.strip().encode('utf-8'))
+    respuesta = cliente.recv(1024).decode('utf-8').strip()
+    if not respuesta.startswith("Usuario Registrado con "):
+        print(respuesta)
+        cliente.close()
+        return
+    
+    hilo_escucha = threading.Thread(target=escuchar_servidor, args=(cliente,), daemon=True)
+    hilo_escucha.start()
+    print(">>> Conectado correctamente a la sala de chat, escribe el comando y apreta enter")
+    while True:
+        try:
+            entrada = input()
+            cliente.send(entrada.encode('utf-8'))
+        except KeyboardInterrupt as e:
+            cliente.send("DISCONNECT".encode('utf-8'))
+            break
+    print("\nDesconectando..")
+    cliente.close
+
+if __name__ == "__main__":
+    iniciar_cliente()
--- main_server/tcp_server.py
+++ main_server/tcp_server.py
@@ -1,31 +1,69 @@
 import socket
 import threading
+from utils import enviarLog
+
+clientes_conectados = {}
+
+def broadcast(mensaje, remitente, instancia_compartida):
+    with instancia_compartida.lock:
+        for usuario, socket_destino in clientes_conectados.items():
+            if usuario != remitente:
+                try:
+                    socket_destino.send(f"[{remitente}]: {mensaje}".encode('utf-8'))
+                except:
+                    pass
+
+def comandoRecibido(comando, username, instancia_compartida):
+    if comando.startswith("MSG "):
+        mensaje = comando[4:]
+        with instancia_compartida.lock:
+            instancia_compartida.history.append({
+                "user": f"{username}",
+                "msg": f"{mensaje}"
+            })
+        enviarLog("MSG", f"{username}: {mensaje}")
+        broadcast(mensaje, username, instancia_compartida)
+        return True
+    elif comando == "DISCONNECT":
+        return False
+    else:
+        enviarLog("ERROR", f"{username}: Comando Desconocido")
+        return False
+
 
 def hilo_usuario(socket_cliente, direccion, instancia_compartida):
     username = None
     try:
         mensaje = socket_cliente.recv(1024)
         if not mensaje:
-            socket_cliente.close()
             return
+        
         mensaje = mensaje.decode('utf-8').strip()
         if mensaje.startswith("NICK "):
             username = mensaje[5:].strip()
             with instancia_compartida.lock:
+                if username in instancia_compartida.users:
+                    socket_cliente.send(f"Ya existe un usuario conectado con ese nick\n".encode('utf-8'))
+                    return
                 instancia_compartida.users.append(username)
-            socket_cliente.send(f"TCP_server: Usuario Registrado con Nick {username}".encode('utf-8'))
-            
+                clientes_conectados[username] = socket_cliente
+            socket_cliente.send(f"Usuario Registrado con Nick {username}".encode('utf-8'))
+            enviarLog("CONNECT",f'usuario = {username}' )
         else:
-            socket_cliente.send("TCP_server:  Para registrarte debes enviar NICK <tu_nombre>\n".encode('utf-8'))
-            socket_cliente.close()
+            socket_cliente.send("Para registrarte debes enviar NICK <tu_nombre>\n".encode('utf-8'))
             return
 
         while True:
-            mensaje = socket_cliente.recv(1024)
-            if not mensaje:
-                socket_cliente.close()
-                return
-
+            entrada = socket_cliente.recv(1024)
+            if not entrada:
+                break
+            comando = entrada.decode('utf-8').strip()
+            estado = comandoRecibido(comando, username, instancia_compartida)
+            if not estado:
+                if comando == "DISCONNECT":
+                    break
+                else:
+                    socket_cliente.send("Comando no reconocido".encode('utf-8'))
     except Exception as e:
         print(f"TCP_server: Error inesperado: {e}")
         return
@@ -35,11 +73,15 @@ def hilo_usuario(socket_cliente, direccion, instancia_compartida):
             with instancia_compartida.lock:
                 if username in instancia_compartida.users:
                     instancia_compartida.users.remove(username)
+                if username in clientes_conectados:
+                    del clientes_conectados[username]
+
+        enviarLog("DISCONNECT", f"{username} se ha desconectado")
         socket_cliente.close()
 
 def tcp_init(instancia_compartida):
     TCP_IP = "127.0.0.1"
-    TCP_PORT = 50002
+    TCP_PORT = 50004
     print(f"TCP_server: Iniciando el tcp en el puerto {TCP_PORT} e ip {TCP_IP}")
     socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket_server.bind((TCP_IP, TCP_PORT))
